1fb3702d5357f27d4e3453bec8e5b9b6fdb7c85c
Darshan  K M <157715317+darshankm-excelr@users.noreply.github.com>
2024-05-18 13:37:45 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,14 @@
+/*const number =[12,45,23,1].filter((number)=>number>10);
+
+console.log(number);
+
+const hello=number.reduce((accumulator,number)=>accumulator+number);
+console.log(hello);
+
+const darshan={name: 'darshan',
+               age:"18",
+               sayhi:()=>{console.log(this.name)}
+};
+darshan.sayhi();*/
+
+
